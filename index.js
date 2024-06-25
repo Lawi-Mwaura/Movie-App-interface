1f45f19466a1f410d9e22742a875ac548b76d714
Lawi-Mwaura <lawimwaura@gmail.com>
2024-06-25 15:48:06 +0300

    Updated css

--- index.js
+++ index.js
@@ -1,42 +1,43 @@
-//Stating my constants
+// Stating my constants
 const apiKey = "b777522a168b4f933547e01643057b97";
 const imgApi = "https://image.tmdb.org/t/p/w500";
-const searchUrl = `https://api.themoviedb.org/3/movie/550?api_key=b777522a168b4f933547e01643057b97&query=`;
+const searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=`;
 const form = document.getElementById("search-form");
 const query = document.getElementById("search-input");
 const result = document.getElementById("result");
-const page = 1;
+let page = 1;
 let isSearching = false;
 
-//Fetching JSON data from url
-async function fetchData(url){
-    try{
+// Fetching JSON data from url
+async function fetchData(url) {
+    try {
         const response = await fetch(url);
-        if(!response.ok){
+        if (!response.ok) {
             throw new Error("Network response was faulty");
         }
         return await response.json();
-    } catch (error){
+    } catch (error) {
         return null;
     }
 }
 
-//Fetch and show results using the url
-async function fetchAndShowResult(url){
+// Fetch and show results using the url
+async function fetchAndShowResult(url) {
     const data = await fetchData(url);
     if (data && data.results) {
         showResults(data.results);
+        isSearching = false; // Reset isSearching after fetching results
     }
 }
 
-//Defining a function that creates the movie cards
+// Defining a function that creates the movie cards
 function createMovieCard(movie) {
     const { poster_path, original_title, release_date, overview } = movie;
     const imagePath = poster_path ? imgApi + poster_path : "./img-01.jpeg";
-    const trunctatedTitle =
-      original_title.length > 15 ? original_title.slice(0, 15) + "..." : original_title;
+    const truncatedTitle =
+        original_title.length > 15 ? original_title.slice(0, 15) + "..." : original_title;
     const formattedDate = release_date || "No release date";
-  
+
     const cardTemplate = `
       <div class="column">
         <div class="card">
@@ -46,7 +47,7 @@ function createMovieCard(movie) {
           <div class="card-content">
             <div class="card-header">
               <div class="left-content">
-                <h3 style="font-weight: 600">${trunctatedTitle}</h3>
+                <h3 style="font-weight: 600">${truncatedTitle}</h3>
                 <span style="color: #12efec">${formattedDate}</span>
               </div>
               <div class="right-content">
@@ -58,50 +59,51 @@ function createMovieCard(movie) {
         </div>
       </div>`;
     return cardTemplate;
-  }
-  
+}
 
-//Clear result element for Search
-function clearResult(){
+// Clear result element for Search
+function clearResult() {
     result.innerHTML = "";
 }
 
-//Display results on the page
- function showResults(item){
-    const newContent = item.map(createMovieCard).join("");
+// Display results on the page
+function showResults(items) {
+    const newContent = items.map(createMovieCard).join("");
     result.innerHTML += newContent || "<p>No results found.</p>";
 }
 
-//Load more results
-async function loadMoreResults(){
-    if (isSearching){
+// Load more results
+async function loadMoreResults() {
+    if (isSearching) {
         return;
     }
+    isSearching = true;
     page++;
     const searchTerm = query.value;
-    const url = searchTerm ? `${searchUrl}${searchTerm}&page=${page}`: `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${apiKey}&page=${page}`;
-    await fetchAndShowResult (url);
+    const url = searchTerm ? `${searchUrl}${searchTerm}&page=${page}` : `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${apiKey}&page=${page}`;
+    await fetchAndShowResult(url);
 }
 
-//Detect end of page and load more results
-function detectEnd(){
-    const {scrollTop, clientHeight, scrollHeight} = document.documentElement;
-    if(scrollTop + clientHeight >= scrollHeight - 20){
+// Detect end of page and load more results
+function detectEnd() {
+    const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
+    if (scrollTop + clientHeight >= scrollHeight - 20) {
         loadMoreResults();
     }
 }
 
-//Handle the Search functionality
-async function handleSearch(e){
-      e.preventDefault();
-      const searchTerm = query.value.trim();
-      if(searchTerm){
+// Handle the Search functionality
+async function handleSearch(e) {
+    e.preventDefault();
+    const searchTerm = query.value.trim();
+    if (searchTerm) {
         isSearching = true;
         clearResult();
+        page = 1; // Reset page to 1 when a new search is initiated
         const newUrl = `${searchUrl}${searchTerm}&page=${page}`;
         await fetchAndShowResult(newUrl);
         query.value = "";
-      }
+    }
 }
 
 // Adding Event listeners
@@ -109,12 +111,12 @@ form.addEventListener('submit', handleSearch);
 window.addEventListener('scroll', detectEnd);
 window.addEventListener('resize', detectEnd);
 
-//Initializing the page
-async function init(){
+// Initializing the page
+async function init() {
     clearResult();
-    const url =`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${apiKey}&page=$(page)`;
+    const url = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${apiKey}&page=${page}`;
     isSearching = false;
     await fetchAndShowResult(url);
 }
 
-init();// Initailizes the page
+init(); // Initializes the page
